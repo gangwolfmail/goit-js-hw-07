18fd07c1d92e8297b0125bd7c066e3540d31bf87
gangwolfmail <129858122+gangwolfmail@users.noreply.github.com>
2023-12-25 15:38:24 +0200

    add task 3, 4, 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const hexColor = document.querySelector(".color");
+
+function setRandomHexColor() {
+  body.style.background = getRandomHexColor();
+  hexColor.textContent = getRandomHexColor();
+}
+
+button.addEventListener("click", setRandomHexColor);
